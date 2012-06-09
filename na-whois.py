9aae103fde0e97130002b3e95489fa323c386037
le087 <mak.tomilov@gmail.com>
2012-06-09 17:23:29 +0600

    first commit

---
+++ na-whois.py
@@ -0,0 +1,26 @@
+# all the imports
+import os
+import sys
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def clear_input():
+    """test input user on valid"""
+    pass
+    
+def get_whois(domain_or_ip):
+    """get whois information of domain or ip"""
+    return os.system('whois %s' % domain_or_ip)
+
+@app.route('/')
+def index():
+    """view for user"""
+    return render_template('main.html')
+
+
+
+
+
+        
