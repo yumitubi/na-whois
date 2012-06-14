45bf60722e1999a235d9c13d67abda964276d30d
Mak Tomilov <mak.tomilov@gmail.com>
2012-06-14 09:43:43 +0600

    ковыряю формы и их обработку\

--- na-whois.py
+++ na-whois.py
@@ -1,4 +1,4 @@
-# all the imports
+# -*- coding: utf-8 -*-
 import os
 import sys
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
@@ -6,19 +6,22 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def clear_input():
-    """test input user on valid"""
-    pass
-    
 def get_whois(domain_or_ip):
     """get whois information of domain or ip"""
     return os.system('whois %s' % domain_or_ip)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     """view for user"""
-    test_whois = get_whois('le087.ru')
-    return render_template('main.html', test_whois)
+    if request.method == 'POST':
+        if request.form['username']:
+            test = 'Информация Whois домена или IP-адреса.'.decode('utf-8')
+            test2 = get_whois(request.form['username'])
+            return render_template('main.html', test=test, test2=test2)
+    else:
+        test = 'Информация Whois домена или IP-адреса.'.decode('utf-8')
+        test2 = 'Пустота'.decode('utf-8')
+        return render_template('main.html', test=test, test2=test2)
 
 if __name__ == "__main__":
     app.run(debug=True)
